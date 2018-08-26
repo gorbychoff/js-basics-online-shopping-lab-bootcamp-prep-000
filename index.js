db1c243ab1a32bfa642247b86aa98a99f8e963ad
Viam Mercer <viam.mercer@hotmail.com>
2018-08-26 10:14:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function total() {
 
 function removeFromCart(item) {
   let item1 = cart.find(n => n.itemName === item);
-  if (item1 != undefined) {
+  if (item1 !== undefined) {
     var i = cart.indexOf(item1); 
     cart.splice(i, 1);
   }
