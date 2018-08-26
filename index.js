e1419475ab5c96915991d00fa71149bc8420a04a
Viam Mercer <viam.mercer@hotmail.com>
2018-08-26 10:19:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
-    let total = total();
+    let grandTotal = total();
     cart = [];
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is $${grandTotal}, which will be charged to the card ${cardNumber}.`;
   }
 }
