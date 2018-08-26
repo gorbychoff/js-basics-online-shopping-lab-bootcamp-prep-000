28f473a645da191e28b9364f1b74c9dafde1fbeb
Viam Mercer <viam.mercer@hotmail.com>
2018-08-26 09:58:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
- var myObject = new Object();
- myObject.itemName = item;
- myObject.itemPrice = Math.floor(Math.random() * (100 - 1)) + 1;
- cart.push(myObject);
- return `${myObject.itemName} has been added to your cart.`;
+  var myObject = new Object();
+  myObject.itemName = item;
+  myObject.itemPrice = Math.floor(Math.random() * (100 - 1)) + 1;
+  cart.push(myObject);
+  return `${myObject.itemName} has been added to your cart.`;
+  
 }
 
 function viewCart() {
@@ -23,19 +23,36 @@ function viewCart() {
     return `Your shopping cart is empty.`;
   }
   else if (cart.length === 1) {
-    return `In your cart, you have ${cart[0].itemName} at $ ${cart[0].itemPrice} `;
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+  }
+  else if (cart.length === 2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
   }
   else {
-    return `In your cart, you have ${cart} `;
+    let i = cart.length;
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}.`;
       }
   }
 
 function total() {
-  // write your code here
+  var total = 0;
+  var i = 0;
+  do {
+    total = total + cart[i].itemPrice;
+    i = i + 1;
+  }
+  while (i < cart.length);
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  if (cart.includes(item)) {
+    var i = cart.indexOf(item); 
+    cart.splice(i, 1);
+  }
+  else {
+    return "That item is not in your cart.";
+  }
 }
 
 function placeOrder(cardNumber) {
